0ea4bbd6da44c593e7f79c29b89446a82d64950b
ПК <petrenkoartem2001@gmail.com>
2024-04-12 16:09:59 +0300

    test3

--- Tests/tests.py
+++ Tests/tests.py
@@ -19,3 +19,16 @@ def test_read_population_data(sample_population_data):
     }
     result = read_population_data(sample_population_data)
     assert result == expected_data
+
+@pytest.mark.parametrize("input_data, expected_output", [
+    ({
+        'Ukraine': [(2000, 48000000), (2010, 46000000)],
+        'USA': [(2000, 281000000), (2010, 309000000)]
+    }, {
+        'Ukraine': [(2010, -2000000)],
+        'USA': [(2010, 28000000)]
+    }),
+])
+def test_calculate_population_change(input_data, expected_output):
+    result = calculate_population_change(input_data)
+    assert result == expected_output
