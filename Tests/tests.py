ca84bf0b5b189e78d3d521c299da43aa6dcf4049
ПК <petrenkoartem2001@gmail.com>
2024-04-12 16:19:09 +0300

    population_data

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,23 +1,24 @@
 import pytest
-
 from tasks import read_population_data, calculate_population_change
 
 @pytest.fixture
-def sample_population_data():
+def sample_population_data_file(tmpdir):
     data = """
     Ukraine, 2000, 48000000
     Ukraine, 2010, 46000000
     USA, 2000, 281000000
     USA, 2010, 309000000
     """
-    return data.strip()
+    file_path = tmpdir.join('population_data.txt')
+    file_path.write(data)
+    return str(file_path)
 
-def test_read_population_data(sample_population_data):
+def test_read_population_data(sample_population_data_file):
     expected_data = {
         'Ukraine': [(2000, 48000000), (2010, 46000000)],
         'USA': [(2000, 281000000), (2010, 309000000)]
     }
-    result = read_population_data(sample_population_data)
+    result = read_population_data(sample_population_data_file)
     assert result == expected_data
 
 @pytest.mark.parametrize("input_data, expected_output", [
