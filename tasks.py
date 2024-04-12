a9087da22d90fa8aa596010475ca1acd5099a27f
ПК <petrenkoartem2001@gmail.com>
2024-04-12 15:51:14 +0300

    population_change

--- tasks.py
+++ tasks.py
@@ -11,8 +11,29 @@ def read_population_data(file_path):
                 population_data[country].append((year, population))
     return population_data
 
+def calculate_population_change(population_data):
+    population_change = {}
+    for country, data in population_data.items():
+        population_change[country] = []
+        prev_population = None
+        for year, population in data:
+            if prev_population is not None:
+                change = population - prev_population
+                population_change[country].append((year, change))
+            prev_population = population
+    return population_change
+
 def main():
     file_path = 'population_data'
     population_data = read_population_data(file_path)
+    population_change = calculate_population_change(population_data)
+
+
+    for country, changes in population_change.items():
+        print(f'Зміна населення для {country}:')
+        for year, change in changes:
+            print(f'\tРік {year}: {change}')
+
+
 if __name__ == "__main__":
     main()
