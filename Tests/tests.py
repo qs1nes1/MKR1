6f01735702ec5973061cd673b2b967a95a9362d1
ПК <petrenkoartem2001@gmail.com>
2024-04-12 15:56:22 +0300

    test1

---
+++ Tests/tests.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tasks import read_population_data, calculate_population_change
+
+@pytest.fixture
+def sample_population_data():
+    # Приклад тестових даних
+    data = """
+    Ukraine, 2000, 48000000
+    Ukraine, 2010, 46000000
+    USA, 2000, 281000000
+    USA, 2010, 309000000
+    """
+    return data.strip()
